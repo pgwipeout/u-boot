889a04915feb9e017aee714c201943ea1f25ec1e
Peter Geis <pgwipeout@gmail.com>
2020-09-02 13:01:16 +0000

    ARM: tegra: Add Ouya board
    Signed-off-by: Peter Geis <pgwipeout@gmail.com>

---
+++ include/configs/ouya.h
@@ -0,0 +1,31 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (c) 2020, Peter Geis <pgwipeout@gmail.com>
+ */
+
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+#include <linux/sizes.h>
+
+#include "tegra30-common.h"
+
+#define CONFIG_TEGRA_BOARD_STRING	"Ouya Game Console"
+
+#define CONFIG_TEGRA_ENABLE_UARTD
+#define CONFIG_SYS_NS16550_COM1		NV_PA_APB_UARTD_BASE
+
+#define CONFIG_SKIP_LOWLEVEL_INIT
+#define CONFIG_PRAM 4096
+
+#define BOARD_EXTRA_ENV_SETTINGS \
+        "reboot_bootloader=mw 7000e450 40000200 ; mw 7000e400 40000210\0" \
+	"devnum=0\0"  \
+	"devtype=mmc\0" \
+	"distro_bootpart=3\0" \
+	"bootcmd_ouya=run scan_dev_for_boot\0"
+
+#include "tegra-common-usb-gadget.h"
+#include "tegra-common-post.h"
+
+#endif /* __CONFIG_H */
